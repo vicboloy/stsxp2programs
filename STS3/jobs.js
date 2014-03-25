30c51853a135f42f01b4b3a4be5a27cc0bbdefcb
Joe Collins <joe26@type6.net>
2014-03-25 09:26:07 +0000

    change in style sheets button visuals kiss import etc

--- STS3/jobs.js
+++ STS3/jobs.js
@@ -1,3 +1,301 @@
+/**
+ * @properties={typeid:35,uuid:"C21C7885-423A-458D-A0AC-E90ACF5D7D37",variableType:-4}
+ */
+var importResults = [];
+/**
+ * @properties={typeid:35,uuid:"C2F4D612-6B67-4D98-A897-BE724811CB53",variableType:-4}
+ */
+var importErrors = [];
+/**
+ * @properties={typeid:35,uuid:"14A210F9-34D2-4D24-8E4E-95AE97FCF96B",variableType:-4}
+ */
+var mappedFormatArray = [];
+/**
+ * @properties={typeid:35,uuid:"1C39F0C7-0609-47D5-AF72-C34E9A51E89F",variableType:-4}
+ */
+var mappedFormatArrayDescrips = [];
+/**
+ * @type {String}
+ *
+ * @properties={typeid:35,uuid:"9B57177F-1D0C-4856-8047-692D87829775"}
+ */
+var importJobNumber = "";
+/**
+ * @type {String}
+ *
+ * @properties={typeid:35,uuid:"31C27CBE-0E9D-4730-A03C-922F0BA707C8"}
+ */
+var importDate = "";
+/**
+ * @type {String}
+ *
+ * @properties={typeid:35,uuid:"9CCA6C50-C08B-48C2-8788-11261608EA40"}
+ */
+var importJobName = "";
+/**
+ * @type {String}
+ *
+ * @properties={typeid:35,uuid:"176425AB-BB7A-45A3-81D2-523AF5D2345C"}
+ */
+var importFormatType = "";
+/**
+ * TODO generated, please specify type and doc for the params
+ * @param file
+ *
+ * @properties={typeid:24,uuid:"542DE6DC-4367-4BAF-A98B-7C199873EA9D"}
+ */
+// -------------------------------------------------------------------------------------------------------------------
+/**
+ * TODO generated, please specify type and doc for the params
+ * @param columnName
+ * @param content
+ *
+ * @properties={typeid:24,uuid:"784F88EE-9CEB-40C4-B720-54D4928FF9C1"}
+ * @AllowToRunInFind
+ */
+function getFieldDataMapping(columnName, content){
+	var fieldMap = "";
+	/** @type {JSFoundSet<db:/stsservoy/mapping>} */
+	var fs = databaseManager.getFoundSet('db:/stsservoy/mapping');
+	if (fs.find()){
+		fs[columnName] = content;
+		fs.mapped_format = "FabSuite";
+		if (fs.search() > 0){
+			var rec = fs.getSelectedRecord();
+			fieldMap = rec.mapping_key+","+rec.field_order+",0";
+			return fieldMap;
+		} else {
+			return "";
+		}
+	}
+}
+/**
+ * @properties={typeid:24,uuid:"A7AE0B04-B8F0-4939-A721-E2698FF530FA"}
+ */
+function existsJobNumber(jobNumber){
+	/** @type {JSFoundSet<db:/stsservoy/jobs>} */
+	var fs = databaseManager.getFoundSet("db:/stsservoy/jobs");
+	if (find()){
+		fs.job_number = jobNumber;
+		if (search()){
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
+/**
+ * @properties={typeid:24,uuid:"A9901D66-1405-4F29-B763-64E296A272BE"}
+ */
+function createBarCode(){
+	
+}
+/**
+ * TODO generated, please specify type and doc for the params
+ * @param file
+ *
+ * @properties={typeid:24,uuid:"82933605-B94C-4B67-979A-6E8B9A0BF807"}
+ */
+function readKissTextFile(file) {
+	var prefs = {
+		delimiter: ",",
+		header:false,
+		dynamicTyping:false
+	}
+	var path = "C:\\Users\\Alienware\\Documents\\STS p2programs\\KISS\\";	//TODO general program preferences
+	if (file == "") {file = path} else {file = file[0]}
+	var fileText = "";
+	//var fileJS = plugins.file.convertToJSFile(file);
+	fileText = plugins.file.readTXTFile(file);
+    
+    importResults =  parse(fileText,prefs);
+/*
+    var length = importResults.results.length;
+    var linePre = "";
+    var rec1 = null;
+    for (var index = 0;index <300;index++){
+    	linePre = index+' ';
+    	rec1 = importResults.results[index];
+    	err1 = importResults.errors[index];
+    	for (var prop in rec1){
+    		linePre = linePre+'||'+rec1[prop];
+    	}
+    	for (var prop2 in err1){
+    		linePre = linePre+" EEE "+err1[prop2].length;
+    	}
+    	application.output(linePre);
+    }
+*/
+    index = idHeaders();
+	dataErrorCheck(index);
+	var mappingDone = loadKissMapping();
+	if (!mappingDone){return}
+	savePiecemarks();
+}
+/**
+ * @AllowToRunInFind
+ *
+ * @properties={typeid:24,uuid:"89287D7F-6E72-4A30-8E10-3D8F4268A46C"}
+ */
+function loadKissMapping(){
+	/** @type {JSFoundSet<db:/stsservoy/mapping>} */
+	var fs5 = databaseManager.getFoundSet("db:/stsservoy/mapping");
+	var mappedFormat = 'FabSuite';
+	mappedFormatArray = [];
+	mappedFormatArrayDescrips = [];
+	var mapLine = [];
+	if (fs5.find()){
+		fs5.mapped_format = mappedFormat;
+		var oldMapTo = "";
+		var mapTo = "";
+		var mapCount = 0;
+		if (fs5.search()){
+			fs5.sort('mapped_format asc, mapping_key asc, field_order asc');
+			var count = fs5.getSize();
+			for (var i6=1;i6 <= count;i6++){
+				var rec6 = fs5.getRecord(i6);
+				mapTo = rec6.mapping_key+','+rec6.field_order;
+				if (mapTo == oldMapTo){mapCount++}else{mapCount = 0}
+				if (rec6.mapped_field == null){continue}  // skip null mappings, won't work
+				mappedFormatArray[mapTo+','+mapCount] = rec6.mapped_field;
+				mappedFormatArrayDescrips[mapTo+','+mapCount] = rec6.field_name;
+				oldMapTo = mapTo;
+			}
+		}
+	}
+	// verify that all provided elements have been mapped, this is an error if any are missed from KISS file
+	// errors in mapping should cancel an import
+	var data = importResults.results;
+	var dataLength = data.length;
+	var needsMap = [];
+	for (i6=1;i6<dataLength;i6++){ // start at line 1 (one) to skip header title
+		var lineNumber = data[i6];	// get line
+		if (lineNumber[0] == "*"){continue} // skip comments
+		var fieldCount = data[i6].length;
+		for (var i7=1;i7<fieldCount;i7++){ // start at index=1 since line name is the prefix and is not mapped
+			var fieldValue = data[i6][i7];
+			if (fieldValue == ""){continue}	// no need to map empty fields
+			var indexName = lineNumber[0]+","+i7+",0"; //at least this field is mapped.  ignore additional mappings, not fatal
+			if (!mappedFormatArray[indexName]){
+				if (!(indexName in needsMap)){
+					needsMap[indexName]="";
+				}
+				//application.output("NOT MAPPED: "+data[i6]);
+			}
+		}
+	}
+	application.output('NOT MAPPED:'+needsMap);
+	return needsMap.length == 0;
+}
+/**
+ * @AllowToRunInFind
+ *
+ * @properties={typeid:24,uuid:"8F81FA19-1380-4635-8F1D-16C3D6AAD275"}
+ */
+function idHeaders(){
+	var format = "";
+	var data = importResults.results;
+	var length = data.length;
+	if (data[0].length >2 && data[0][2].search("FabSuite") != -1){format = "FabSuite"}
+	application.output('File Format '+format);
+	var index = 1;
+	while (data[index][0] == "*"){ // get headers if 
+		//application.output(data[index][1]);
+		var headLineSize = data[index].length;
+		if (headLineSize <= 1) {continue}
+		var mappingLine = data[index][1];
+		var fieldName = ""; 	//compare to other non-header lines in file
+		var mappingLineIndex = mappingLine.search(RegExp('\\b[A-Z\\*]\\b'));		///get regexp of 'x' 1 char
+		if (mappingLineIndex != -1){
+			var mappingKey = mappingLine[mappingLineIndex];
+		}
+		for (var i3=1;i3 < headLineSize;i3++){
+			//create records for missing mapping fields under mapping format
+			/** @type {JSFoundset<db:/stsservoy/mappings> */
+			var fs2 = databaseManager.getFoundSet("db:/stsservoy/mapping");
+			//var fs2 = sts_mapping;
+			fieldName = data[index][i3].trim();
+			if (fieldName.search(RegExp('[A-Za-z]+')) == -1){fieldName = "UNDEFINED";}
+			var fieldPosition = i3;
+			//if (mappingLine == fieldName){
+			//	application.output('field name same '+mappingLine);
+			//}
+			//application.output('mapping format: '+format+', Mapping key: '+mappingKey+', Line: '+mappingLine+', index: '+fieldPosition+', field name: '+fieldName);
+			if (fs2.find()){
+				fs2.mapped_format = format;
+				fs2.mapping_line = mappingLine;
+				fs2.field_name = fieldName;
+				if (!fs2.search()){
+					fs2.newRecord();
+					var rec = fs2.getSelectedRecord();
+					//application.output('rec '+rec);
+					rec.mapped_format = format;
+					rec.mapping_line = mappingLine;
+					rec.mapping_key = mappingKey;
+					if (mappingLine == fieldName){
+						rec.field_value = mappingKey;
+					}
+					rec.field_name = fieldName;
+					rec.field_order = fieldPosition-1;
+					databaseManager.saveData(rec);
+					//application.output('format '+rec.mapped_format+ ' '+rec.field_name);
+				}
+			}
+		}
+		index++;		
+		//var fs9 = sts_mapping;
+		//application.output('mapping lines: '+fs9.getSize());	
+	}
+
+	return index;
+
+
+}
+/**
+ * @AllowToRunInFind
+ * 
+ * TODO generated, please specify type and doc for the params
+ * @param index
+ *
+ * @properties={typeid:24,uuid:"CE27525E-3CCF-4431-B250-555756249AE8"}
+ */
+function dataErrorCheck(index) {
+ 	var data = importResults.results;
+	//var index2 = index;
+	var length2 = length < 3000 ? length : length*.10;
+	var lines = [];
+	var lineType = "";
+	var typeFieldCount = 0;
+	for (var i = index;i < length; i++){ // get sample line types and sizes
+		lineType = data[i][0];
+		typeFieldCount = data[i].length;
+		if (lines[lineType] == null){
+			lines[lineType] = typeFieldCount
+			//application.output('Line '+lineType+' '+typeFieldCount)
+		} else {
+			if (lines[lineType] != typeFieldCount){
+				application.output('Found '+lineType+' '+typeFieldCount+' but had '+lines[lineType]+' fields')
+			}
+		}
+	}
+	// Update key code temp
+	/** @type {JSFoundSet<db:/stsservoy/mapping>} */
+ 	var fs3 = databaseManager.getFoundSet("db:/stsservoy/mapping");
+ 	
+	fs3.loadRecords();
+	var fs3Size = fs3.getSize();
+	for (var i4 = 1;i4 <= fs3Size;i4++){
+		var rec2 = fs3.getRecord(i4);
+		if (rec2.mapping_key == null) {
+			var keySource = rec2.mapping_line;
+			var keyIndex = keySource.search(RegExp('\\b[A-Z\\*]\\b'));
+			var key = rec2.mapping_line[keyIndex];
+			rec2.mapping_key = key;
+			databaseManager.saveData(rec2);
+			application.output('KEY SEARCH. Line: '+rec2.mapping_line+', key: '+key);
+		}
+	}
+}
 
 
 /*
@@ -5,7 +303,7 @@
  v2.0.3
  https://github.com/mholt/jquery.parse
  */
-;
+
 	/**
  * TODO generated, please specify type and doc for the params
  * @param e
@@ -15,83 +313,83 @@
 function t(e) {
 		return typeof e === "function"
 	}
-	/**
+/**
  * @properties={typeid:24,uuid:"905EDD6A-24B2-4341-82E2-3000B6D6AD42"}
  */
 function savePiecemarks(){
-		var data = forms.kiss_import.importResults.results;
-		var mappedFormatArray = forms.kiss_import.mappedFormatArray;
-		/** @type {JSFoundSet<db:/stsservoy/piecemarks>} */
-		var fs5 = databaseManager.getFoundSet("db:/stsservoy/piecemarks");
-		//var fs5 = sts_piecemark;
-		fs5.loadAllRecords();
-		fs5.deleteAllRecords();//temporary
-		application.output('record piecemark count: '+fs5.getSize());
-		var length = data.length;
-		var skipHeader = true;
-		var fieldCount = 0;
-		var lineArray = [];
-		var mapTo2 = "";
-		var newRecField = "D";
-		var recCount = 0;
-		var newRec = "";
-		var newRecIndex = 0;
-		// mappedFormatArray['mapping_key_line','field_order','0..1..2'] is database.field
-		// data[line_in_file], data array sized to number of fields on the line
-		for (var i6 = 1;i6<length;i6++){
-			//if (i6 > 30){break}
-			skipHeader = skipHeader && data[i6][0] == "*";
-			if (skipHeader){continue}
-			if (data[i6][0] == null){continue}
-			if (data[i6][0] == newRecField){
-				recCount++;
-				application.output('new record');
-				newRecIndex = fs5.newRecord();
-				newRec = fs5.getRecord(newRecIndex);
-				newRec.tenant_uuid = globals.secCurrentTenantID;
-				newRec.sheet_id = globals.secCurrentTenantID;//temporary
-				//continue;
-			}
-			lineArray = data[i6];
-			fieldCount = lineArray.length;
-			var mapIndex = 0;
-			var skip = "";
-			var mappings = [];
-			for (var i = 1;i<fieldCount;i++){
-				if (lineArray[i] == ""){continue}
-				mapTo2 = lineArray[0]+','+i;
-				mapIndex = 0;
-				mappings = [];
-				
-				var fieldValue = "";
-				while (mapTo2+','+mapIndex in mappedFormatArray) {
-					//mapTo2 = lineArray[0]+','+i;
-					
-					if (i6<500){
-						application.output('   line: '+skip+lineArray[0]+' value: '+lineArray[i]+', maps to field: '+mappedFormatArray[mapTo2+','+mapIndex]+', map '+mapTo2+','+mapIndex);
-					}
-					mappings.push(mappedFormatArray[mapTo2+','+mapIndex]); //for ordering of +1 field values after exit this while
-					mapIndex++;
-				}
-				for (var i2=0;i2<mappings.length;i2++){
-					if (!(i2 in mappings)){break}
-					application.output(lineArray);
-					var field = mappings[i2].split('.');
-					fieldValue = newRec[field[1]];
-					if (newRec[field[1]] == null){
-						newRec[field[1]] = lineArray[i];
-					} else {
-						newRec[field[1]] = newRec[field[1]]+' '+lineArray[i];
-					}
-					application.output('newRec field: '+field[1]+' newRec[field]: '+newRec[field[1]]);
-				}
-			}
-			application.output(newRec);
+	var data = scopes.jobs.importResults.results;
+	var mappedFormatArray = forms.kiss_import.mappedFormatArray;
+	/** @type {JSFoundSet<db:/stsservoy/piecemarks>} */
+	var fs5 = databaseManager.getFoundSet("db:/stsservoy/piecemarks");
+	fs5.loadAllRecords();
+	application.output('record piecemark count: '+fs5.getSize());
+	fs5.deleteAllRecords();//temporary
+	application.output('record piecemark count (delete): '+fs5.getSize());
+	var length = data.length;
+	var skipHeader = true;
+	var fieldCount = 0;
+	var lineArray = [];
+	var mapTo2 = "";
+	var newRecField = "D";
+	var recCount = 0;
+	var newRec = "";
+	var newRecIndex = 0;
+	// mappedFormatArray['mapping_key_line','field_order','0..1..2'] is database.field
+	// data[line_in_file], data array sized to number of fields on the line
+	for (var i6 = 1;i6<length;i6++){
+		//if (i6 > 30){break}
+		skipHeader = skipHeader && data[i6][0] == "*";
+		if (skipHeader){continue}
+		if (data[i6][0] == null){continue}
+		if (data[i6][0] == newRecField){
+			recCount++;
+			//application.output('new record');
+			newRecIndex = fs5.newRecord();
+			newRec = fs5.getRecord(newRecIndex);
+			newRec.tenant_uuid = globals.secCurrentTenantID;
+			newRec.sheet_id = globals.secCurrentTenantID;//temporary
+			//continue;
 		}
-		application.output('new Record count: '+recCount);
-	}
+		lineArray = data[i6];
+		fieldCount = lineArray.length;
+		var mapIndex = 0;
+		var skip = "";
+		var mappings = [];
+		for (var i = 1;i<fieldCount;i++){
+			if (lineArray[i] == ""){continue}
+			mapTo2 = lineArray[0]+','+i;
+			mapIndex = 0;
+			mappings = [];
 
-	/**
+			var fieldValue = "";
+			while (mapTo2+','+mapIndex in mappedFormatArray) {
+				//mapTo2 = lineArray[0]+','+i;
+
+				if (i6<0){
+					application.output('   line: '+skip+lineArray[0]+' value: '+lineArray[i]+', maps to field: '+mappedFormatArray[mapTo2+','+mapIndex]+', map '+mapTo2+','+mapIndex);
+				}
+				mappings.push(mappedFormatArray[mapTo2+','+mapIndex]); //for ordering of +1 field values after exit this while
+				mapIndex++;
+			}
+			for (var i2=0;i2<mappings.length;i2++){
+				if (!(i2 in mappings)){break}
+				application.output(lineArray);
+				var field = mappings[i2].split('.');
+				fieldValue = newRec[field[1]];
+				if (newRec[field[1]] == null){
+					newRec[field[1]] = lineArray[i];
+				} else {
+					newRec[field[1]] = newRec[field[1]]+' '+lineArray[i];
+				}
+				//application.output('newRec field: '+field[1]+' newRec[field]: '+newRec[field[1]]);
+			}
+		}
+		//application.output(newRec);
+	}
+	application.output('new Record count: '+recCount);
+}
+
+/**
  * TODO generated, please specify type and doc for the params
  * @param e
  * @param t
@@ -148,7 +446,7 @@ function n(e, t) {
 			c()
 		};
 	}
-	/**
+/**
  * TODO generated, please specify type and doc for the params
  * @param e
  *
@@ -441,6 +739,7 @@ function r(e) {
 		var n = new r(t);
 		return n.parse(e)
 	}
+	
 	/**
  * TODO generated, please specify type and doc for the params
  * @param e
